bde7f3553727e091dc5f26ea493556be42b35df5
Edin <edinomerovic2015@gmail.com>
2023-01-25 00:18:52 +0100

    Refactoring to make it work

--- train.py
+++ train.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 import time
 import argparse
 from model import DeepNeuralNetwork
+from sklearn.model_selection import train_test_split
 
 # Settings
 parser = argparse.ArgumentParser(description='Neural Networks from Scratch')
 parser.add_argument('--activation', action='store', dest='activation', required=False, default='sigmoid', help='activation function: sigmoid/relu')
-parser.add_argument('--batch_size', action='store', dest='batch_size', required=False, default=128)
+parser.add_argument('--batch_size', action='store', dest='batch_size', required=False, default=7)
+parser.add_argument('--epochs', action='store', dest='epochs', required=False, default=128)
 parser.add_argument('--optimizer', action='store', dest='optimizer', required=False, default='momentum', help='optimizer: sgd/momentum')
 parser.add_argument('--l_rate', action='store', dest='l_rate', required=False, default=1e-3, help='learning rate')
 parser.add_argument('--beta', action='store', dest='beta', required=False, default=.9, help='beta in momentum optimizer')
@@ -34,34 +36,29 @@ def one_hot(x, k, dtype=np.float32):
 def main():
     # Load data
     print("Loading data...")
-    mnist_data = fetch_openml("mnist_784")
-    x = mnist_data["data"]
-    y = mnist_data["target"]
+    mnist_data = fetch_openml("mnist_784", version=1, return_X_y=True, as_frame=False, parser="pandas")
+    x = mnist_data[0] # Training set
+    y = mnist_data[1] # Labels
 
     # Normalize
     print("Preprocessing data...")
-    x /= 255.0
+    x = x.astype('float64') / 255.0
 
     # One-hot encode labels
     num_labels = 10
-    examples = y.shape[0]
     y_new = one_hot(y.astype('int32'), num_labels)
 
-    # Split, reshape, shuffle
-    train_size = 60000
-    test_size = x.shape[0] - train_size
-    x_train, x_test = x[:train_size], x[train_size:]
-    y_train, y_test = y_new[:train_size], y_new[train_size:]
-    shuffle_index = np.random.permutation(train_size)
-    x_train, y_train = x_train[shuffle_index], y_train[shuffle_index]
+    # Split data into train partition and test partition
+    x_train, x_test, y_train, y_test = train_test_split(x, y_new, random_state=0, test_size=0.3)
+
     print("Training data: {} {}".format(x_train.shape, y_train.shape))
     print("Test data: {} {}".format(x_test.shape, y_test.shape))
-#     show_images(x_train)
 
     # Train
     print("Start training!")
     dnn = DeepNeuralNetwork(sizes=[784, 64, 10], activation=args.activation)
-    dnn.train(x_train, y_train, x_test, y_test, 
+
+    dnn.train(x_train, y_train, x_test, y_test, epochs=int(args.epochs),
               batch_size=int(args.batch_size), optimizer=args.optimizer, l_rate=float(args.l_rate), beta=float(args.beta))
     
 if __name__ == '__main__':
